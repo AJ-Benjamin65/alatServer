3af2b6ab33382d794e3afa7f67e7e9bfb3d6a097
AJ-Benjamin65 <ajahbenjamin1@gmail.com>
2025-07-19 10:58:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors());
 const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
-const userEmail = "ajaeroelvis4@gmail.com";
-const pass = "ylxnobprypgjidpw";
+const userEmail = "ajahbenjamin1@gmail.com";
+const pass = "jtyrbcfmzfdqabch";
 // 15 APRIL
 
 // Middleware
